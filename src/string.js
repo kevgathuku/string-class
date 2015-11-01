71a928ca675b3a3bca4d5ac60966bcfedfa241bf
Kevin Ndung'u <kevgathuku@gmail.com>
2015-11-01 17:19:23 +0300

    Implement string prototype toLower method

--- src/string.js
+++ src/string.js
@@ -7,18 +7,33 @@
   };
 
   String.prototype.toUpper = function() {
-    var _final = '';
+    var _upper = '';
     for (let i = 0; i < this.length; i++) {
       // Check if the character is a lowercase letter
       if (/[a-z]/.test(this[i])) {
-        // Get the uppercase version of the letter (current  - 32)
+        // Get the uppercase version of the letter (current - 32)
         // and append it to the output string
-        _final += String.fromCharCode(this.charCodeAt(i) - 32);
+        _upper += String.fromCharCode(this.charCodeAt(i) - 32);
       } else {
-        _final += this[i];
+        _upper += this[i];
       }
     }
-    return _final;
+    return _upper;
+  };
+
+  String.prototype.toLower = function() {
+    var _lower = '';
+    for (let i = 0; i < this.length; i++) {
+      // Check if the character is an uppercase letter
+      if (/[A-Z]/.test(this[i])) {
+        // Get the lowercase version of the letter (current + 32)
+        // and append it to the output string
+        _lower += String.fromCharCode(this.charCodeAt(i) + 32);
+      } else {
+        _lower += this[i];
+      }
+    }
+    return _lower;
   };
 
 })();
