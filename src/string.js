22d1f4878c6af30feb351f8e7004ad0c4a5647a9
Kevin Ndung'u <kevgathuku@gmail.com>
2015-11-01 17:17:21 +0300

    Implement toUpper string prototype method

--- src/string.js
+++ src/string.js
@@ -5,4 +5,20 @@
   String.prototype.hasVowels = function() {
     return /[aeiouAEIOU]/.test(this);
   };
+
+  String.prototype.toUpper = function() {
+    var _final = '';
+    for (let i = 0; i < this.length; i++) {
+      // Check if the character is a lowercase letter
+      if (/[a-z]/.test(this[i])) {
+        // Get the uppercase version of the letter (current  - 32)
+        // and append it to the output string
+        _final += String.fromCharCode(this.charCodeAt(i) - 32);
+      } else {
+        _final += this[i];
+      }
+    }
+    return _final;
+  };
+
 })();
